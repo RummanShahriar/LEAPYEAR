42d879f9c6713409bf706bf00e11468bfaee00ef
RummanShahriar <rummanshahriarrumman@gmail.com>
2023-01-15 02:02:49 +0600

    first commit

---
+++ playsound.py
@@ -0,0 +1,2 @@
+from playsound import playsound
+playsound('"D:\\[FreeCourseSite.com] Udemy - 100 Days Of Code 2023 Web Development Bootcamp\\Poth-Chola-Artcell.mp3"\\play.mp3')
---
+++ slice.py
@@ -0,0 +1,7 @@
+a = "Rumman"
+print(len(a))
+b = a[1:5]
+print(b)
+print(a[0:6:2])
+print(a[::-1])
+print(a[-1:-4:-1])
